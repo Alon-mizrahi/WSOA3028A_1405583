a1aedd070ea44e9289ceffff64e2bd7fb61cc485
Alon-mizrahi <alonmiz1@gmail.com>
2020-03-09 18:37:01 +0200

    quick change to index.js test test test

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 
 
-let a=document.getElementById("mybutton");
-a.addEventListener('click',func);
-function func()
-{
+let a = document.getElementById("mybutton");
+a.addEventListener('click', func);
+function func() {
     alert("big PP");
 }
 
 
 //let backBut = document.getElementById("BackButton");
 //backBut.addEventListener('click', BackFunc);
-function BackFunc()
-{
+function BackFunc() {
     window.history.back();
 }
 
