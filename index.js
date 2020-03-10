1a32b83d0e739b0b60fe220503970fa42c7b82ec
Alon-mizrahi <alonmiz1@gmail.com>
2020-03-10 22:03:42 +0200

    added three conspiricies to the main page. want to add images to acompany the stories soon. started on the lizard game. currently testing.

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ function func() {
     alert("big PP");
 }
 
-
-//let backBut = document.getElementById("BackButton");
-//backBut.addEventListener('click', BackFunc);
 function BackFunc() {
     window.history.back();
 }
 
 
+
+//Lizard people game-----------------------------------------
+
+imagesAr =
+    ["https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg",
+        "https://www.thesprucepets.com/thmb/wpN_ZunUaRQAc_WRdAQRxeTbyoc=/4231x2820/filters:fill(auto,1)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
+        "https://cdn1-www.dogtime.com/assets/uploads/2019/07/PuppyEyes1.jpg",
+        "https://www.readersdigest.ca/wp-content/uploads/sites/14/2019/02/shutterstock_673465372.jpg",
+        "https://i2.wp.com/metro.co.uk/wp-content/uploads/2017/10/567892431.jpg?quality=90&strip=all&zoom=1&resize=644%2C429&ssl=1",
+        "https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg"
+    ];
+
+let imagestack = document.getElementById("LizardGameStack");
+
+let n = 0;
+function Nextimage(imagesAr) {
+    imagestack.src = imagestack[n];
+    n++;
+}
+
+//-----------------------------------------------------------
