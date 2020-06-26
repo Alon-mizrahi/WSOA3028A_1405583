56abc43c73139e0d2ec29d289a72497ab95362ee
Alon-mizrahi <alonmiz1@gmail.com>
2020-06-26 23:00:14 +0200

    testing news api. dont think it works on local host. will try debug from console without liveserver

---
+++ newshandler.js
@@ -0,0 +1,11 @@
+//here is where i will handle news board api
+var url = 'http://newsapi.org/v2/top-headlines?' +
+    'country=za&' +
+    'apiKey=49d1cde63e504a929606432ea8c113c4';
+var req = new Request(url);
+fetch(req)
+    .then(function (response) {
+        console.log(response.json());
+    })
+
+alert('wat');
