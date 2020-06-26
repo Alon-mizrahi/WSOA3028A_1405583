6e65ca9630e9b62d0c02f656fe5aa486292fbea4
Alon-mizrahi <alonmiz1@gmail.com>
2020-06-26 23:56:38 +0200

    debugging fetch

--- newshandler.js
+++ newshandler.js
@@ -1,5 +1,5 @@
 //here is where i will handle news board api
-var url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=49d1cde63e504a929606432ea8c113c4';
+var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=49d1cde63e504a929606432ea8c113c4';
 var req = new Request(url);
 fetch(req)
     .then(function (response) {
