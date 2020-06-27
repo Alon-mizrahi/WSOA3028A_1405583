58e098850218d030be85196ba34c07f7c6aa815c
Alon-mizrahi <alonmiz1@gmail.com>
2020-06-27 13:50:34 +0200

    debug

--- newshandler.js
+++ newshandler.js
@@ -7,7 +7,7 @@ alert('wat');
 
 $(document).ready(function () {
 
-    var url = "http://newsapi.org/v2/top-headlines?country=za&apiKey=49d1cde63e504a929606432ea8c113c4";
+    var url = "https://newsapi.org/v2/top-headlines?country=za&apiKey=49d1cde63e504a929606432ea8c113c4";
 
     console.log(url);
 
