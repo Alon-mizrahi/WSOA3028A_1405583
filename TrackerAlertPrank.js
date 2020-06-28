183aaad1214ebb2d6c8fd2702cb07286359ad688
Alon-mizrahi <alonmiz1@gmail.com>
2020-06-28 23:01:17 +0200

    Added last blog post. API working fully. added govrn tracker prank to index page. added referance list blog.

--- TrackerAlertPrank.js
+++ TrackerAlertPrank.js
@@ -1,1 +1,32 @@
-alert("hello Alon");
+function myFunction() {
+    var myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+    //pop up window joke-------------------------------------
+    //need geo location and send alert
+    window.onload = function geofind() {
+        alert("the government is trying to track you!!");
+
+        function success(position) {
+            alert("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude + " We know where you are");
+        }
+        function error() {
+            if (error.code == error.PERMISSION_DENIED) {
+                alert("phew... were safe for now");
+            }
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+if (!localStorage.getItem("visted")) {
+    myFunction();
+    localStorage.setItem("visted", true);
+}
+
+
+//this joke only works the first time a user loads the index.html page, as to not irritate users. 
+//to have the popups come up again clear cookies and cache
+//also reset default permissions for the site.
+//ie: location -> ask by default
